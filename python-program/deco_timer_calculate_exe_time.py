14c9936e59aed26992134a8c8663e1aa5fa90efe
ari <abhay.santra@gmail.com>
2019-09-04 09:49:35 +0530

    python program added

---
+++ python-program/deco_timer_calculate_exe_time.py
@@ -0,0 +1,22 @@
+import functools
+import time
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func) # used to preserve identity of waste_some_time()
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+@timer
+def waste_some_time(num_times):
+    for _ in range(num_times):  # here _ used as this variable will not be used in future
+        return sum([i**2 for i in range(10000)])
+
+ret_val = waste_some_time(1)
+print(f"return value : {ret_val}")
---
+++ python-program/decorator.py
@@ -0,0 +1,50 @@
+import os, sys
+
+print('.........decorator without arguments..............')
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_whee():
+    print("Whee!")
+
+# @my_decorator => say_whee = my_decorator(say_whee)
+say_whee()
+print('.....................decorator with arguments.........')
+
+def my_decorator_with_arg(func):
+    def wrapper(*args, **kwargs):
+        print("Something is happening before the function is called.")
+        func(*args, **kwargs)
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator_with_arg
+def say_whee_with_arg(arg,kwargs):
+    print("Whee! ",arg, kwargs)
+
+dict_data = {'1':'Monday', '2':'Sunday', '3':'Tuesday'}
+dict_data1 = {'1':'abhay','2':'Bisu', '3':'Kaka'}
+# @my_decorator => say_whee = my_decorator(say_whee)
+say_whee_with_arg("I am in this world !!!", dict_data)
+
+print('................decorator with return values............')
+def my_decorator_with_arg_and_return_val(func):
+    def wrapper(*args, **kwargs):
+        print("before the function is called.")
+        return func(*args, **kwargs)
+        print("after the function is called.")
+    return wrapper
+
+@my_decorator_with_arg_and_return_val
+def say_whee_ret_val(arg,kwargs):
+    print("Whee! ",arg, kwargs)
+    print(f"Hello............: {arg}..{kwargs}")
+    return arg
+
+ret_val = say_whee_ret_val("I am in this world !!!", dict_data)
+print('Ret Val : ',ret_val)
